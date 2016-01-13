2e8b5896e33a25393845687b9914ad1b2534d138
Alexander Doloz <doloz.alexander@gmail.com>
2016-01-13 22:54:18 +0300

    Create STTwitterManager

---
+++ SebbiaTest/STTweet.swift
@@ -0,0 +1,14 @@
+//
+//  STTweet.swift
+//  SebbiaTest
+//
+//  Created by Alexander on 13.01.16.
+//  Copyright © 2016 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class STTweet: NSObject {
+    var date: NSDate!
+    var text: String!
+}
---
+++ SebbiaTest/STTwitterManager.swift
@@ -0,0 +1,17 @@
+//
+//  STTwitterManager.swift
+//  SebbiaTest
+//
+//  Created by Alexander on 13.01.16.
+//  Copyright © 2016 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class STTwitterManager: NSObject {
+    let token = "Bearer AAAAAAAAAAAAAAAAAAAAADiJRQAAAAAAt%2Brjl%2Bqmz0rcy%2BBbuXBBsrUHGEg%3Dq0EK2aWqQMb15gCZNwZo9yqae0hpe2FDsS92WAu0g"
+    
+    func requestTweetsWithHashtag(hashtag: String, completion: ([STTweet]?, NSError?) -> Void) {
+        
+    }
+}
